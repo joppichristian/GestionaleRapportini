c8c6960e8cc286f41aa7b13055f7ee709b857045
LucaCasna <l.casna@gmail.com>
2015-12-28 19:28:41 +0100

    cambio db

--- script_php/connessione-db.php
+++ script_php/connessione-db.php
@@ -1,8 +1,8 @@
 <?php
 
 //$mysqli = new mysqli("54.201.175.207","root","root","gestionale");
-$mysqli = new mysqli("62.149.150.245", "Sql902302", "5a6xps69a6", "Sql902302_2");
-   
+$mysqli = new mysqli("62.149.150.245", "Sql902302", "5a6xps69a6", "Sql902302_4");
+
 if ($mysqli->connect_errno) {
 	echo "Failed to connect to DataBase...";
 	exit();
