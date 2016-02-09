1040ea83653e27a0649c19d5896de1d1a9d8d657
joppichristian <joppi94@gmail.com>
2016-02-09 13:25:03 +0100

    stampa

--- js/stampa_rapportino_dipendente.js
+++ js/stampa_rapportino_dipendente.js
@@ -8,29 +8,24 @@ $(document).ready(function(){
       function downloadFile(fileName, urlData) {
 
         var aLink = document.createElement('a');
-        var evt = document.createEvent("HTMLEvents");
-        evt.initEvent("click");
         aLink.download = fileName;
         aLink.href = urlData;
-        aLink.dispatchEvent(evt);
+        aLink.click();
       }
 
-      var data = '"Column One","Column Two","Column Three"';
-      downloadFile('2.csv', 'data:text/csv;charset=UTF-8,' + encodeURIComponent(data));
-/*
+     /* var data = '"Column One";"Column Two";"Column Three"';*/
+      
+
       var data = [['1st title', '2nd title', '3rd title', 'another title'], ['a a a', 'bb\nb', 'cc,c', 'dd"d'], ['www', 'xxx', 'yyy', 'zzz']];
       var csvContent = "data:text/csv;charset=utf-8,";
-
-      data.forEach(function(infoArray, index){
-        dataString = infoArray.join(",");
-        csvContent += index < data.length ? dataString+ "\n" : dataString;
-      });
+	  var toPrint=csvContent;
+      for(var i=0;i<data.length;i++){
+	      toPrint += data[i].join(';');
+	      toPrint += '\n';
+      }
       var encodedUri = encodeURI(csvContent);
-      var link = document.createElement("a");
-      link.setAttribute("href", encodedUri);
-      link.setAttribute("download", "my_data.csv");
-      link.click();
 
-    });*/
+	  downloadFile('2.csv', toPrint);
+    });
 
 });
